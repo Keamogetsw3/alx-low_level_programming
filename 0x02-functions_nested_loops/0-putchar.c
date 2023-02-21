fa17f8b90c30ca235bd8929e079e4cb68c8a9aec
keamogetswe3 <keamogetswegp@gmail.com>
2023-02-21 10:48:52 -0800

    c - functions and nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *main - Write a program that prints _putchar, followed by a new line
+ *Return: Always 0
+*/
+int main(void) {//print the following
+  printf("_putcher\n");
+  return 0;
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/**
+ * main: print_alphabet - in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main()
