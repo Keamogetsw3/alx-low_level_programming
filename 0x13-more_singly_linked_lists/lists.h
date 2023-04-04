af2daab4e4c6e6eac8d618cfa6167f154676be5a
keamogetswe3 <keamogetswegp@gmail.com>
2023-04-04 15:14:02 -0700

    fixes to lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,20 +18,20 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(const listint_t *h); //prints all the elements of a listint_t list
-size_t listint_len(const listint_t *h); //returns the number of elements in a linked listint_t list
-listint_t *add_nodeint(listint_t **head, const int n); //adds a new node at the beginning of a listint_t list
-listint_t *add_nodeint_end(listint_t **head, const int n); //adds a new node at the end of a listint_t list
-void free_listint(listint_t *head); //frees a listint_t list
-void free_listint2(listint_t **head); //frees a listint_t list
-int pop_listint(listint_t **head); //deletes the head node of a listint_t linked list, and returns the head node’s data (n)
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index); //returns the nth node of a listint_t linked list
-int sum_listint(listint_t *head); //returns the sum of all the data (n) of a listint_t linked list
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n); //inserts a new node at a given position
-int delete_nodeint_at_index(listint_t **head, unsigned int index); //deletes the node at index index of a listint_t linked list
-listint_t *reverse_listint(listint_t **head); // reverses a listint_t linked list
-size_t print_listint_safe(const listint_t *head); //prints a listint_t linked list
-size_t free_listint_safe(listint_t **h); //frees a listint_t list
-listint_t *find_listint_loop(listint_t *head); //finds the loop in a linked list
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif /* LISTS_H */
