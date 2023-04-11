500b7e291d16839168c926c86b8b94930318a4f3
keamogetswe3 <keamogetswegp@gmail.com>
2023-04-10 18:11:25 -0700

    final changes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,63 +2,62 @@
 #include <stdlib.h>
 #include "main.h"
 #define BUF_SIZE 1024
-/**                                                                                                
- * cp file_from file_to - program that copies content of a file 1 to file 2                  
+/**                             
+ * cp file_from file_to - program that copies content of a file 1 to file 2
  * @file: is the name of the file                                   
- * Return: 0 on success                                                               
+ * Return: 0 on success
  */
-void print_error(int error_code, const char *format, const char *file)
-{
-   dprintf(STDERR_FILENO, format, file);
-   exit(error_code);
-}
-
 int main(int argc, char *argv[])
 {
-    int fd_out;
-    int fd_in;
+    int fd_o;
+    int fd_i;
     int wrt;
+    int rd;
+    char buffer[BUF_SIZE];
 
     if (argc != 3)
     {
-        print_error(97, "Usage: %s cp file_from file_to\n", argv[0]);
+       dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+   exit(97); 
     }
-    fd_in = open(argv[1],  O_RDONLY);
-    if (fd_in == -1)
+    fd_i = open(argv[1],  O_RDONLY);
+    if (fd_i == -1)
     {
-        print_error(98, "Error: Can't write to NAME_OF_THE_FILE%s\n", argv[1]);
+      dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+   exit(98);  
     }
-    fd_out = open(argv[2],  O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (fd_out == -1)
+    fd_o = open(argv[2],  O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_o == -1)
     {
-        print_error(99, "Error: Can't write to %s\n", argv[2]);
+      dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+   exit(99);  
     }
-    char buffer[BUF_SIZE];
-    ssize_t rd;
-    while ((rd = read(fd_in, buffer, BUF_SIZE)) > 0)
+    while ((rd = read(fd_i, buffer, BUF_SIZE)) > 0)
     {
-        ssize_t wrt = write(fd_out, buffer, rd);
+        wrt = write(fd_o, buffer, rd);
         if (wrt == -1)
         {
-            print_error(99, "Error: Can't write to %s\n", argv[2]);
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
         }
-        
-    }
-    if (wrt != rd)
-    {
-        print_error(99, "Error: Partial write to %s\n", argv[2]);
+        if (wrt != rd)
+        {
+            dprintf(STDERR_FILENO, "Error: Partial write to %s\n", argv[2);
+            exit(99);
+        }
     }
     if (rd == -1)
     {
         print_error(98, "Error: Can't read from file %s\n", argv[1]);
     }
-    if (close(fd_in) == -1)
+    if (close(fd_i) == -1)
     {
-        print_error(100, "Error: Can't close fd %d\n", fd_in);
+        print_error(100, "Error: Can't close fd %d\n", fd_i);
     }
-    if (close(fd_out) == -1)
+    if (close(fd_o) == -1)
     {
-        print_error(100, "Error: Can't close fd %d\n", fd_out);
+        print_error(100, "Error: Can't close fd %d\n", fd_o);
     }
-    return 0;
+    
+    return (0);
 } 
