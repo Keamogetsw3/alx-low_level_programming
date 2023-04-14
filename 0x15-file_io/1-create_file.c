fc31155cede6a183a60c60c4f447f7103bb72fd7
keamogetswe3 <keamogetswegp@gmail.com>
2023-04-14 15:15:30 -0700

    fixes to the betty syle and doc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,34 @@
 #include "main.h"
-/**                                                                             
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: is the name of the file to create                                 
- * @text_content:  is a NULL terminated string to write to the file             
- * Return: 1 on success, -1 on failure                                          
+
+/**
+ * create_file - read file and prints to POSIX standard output
+ * @filename: is the name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
+  int fd;
     int wrt;
     int len = 0;
-        
+
     if (filename == NULL)
-        return (-1);
+      return (-1);
     fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
     if (fd == -1)
-        return (-1);
+      return (-1);
     if (text_content != NULL)
     {
-        for (len = 0; text_content[len];)
-            len++;
+      for (len = 0; text_content[len];)
+	len++;
     }
     wrt = write(fd, text_content, len);
     if (wrt == -1)
     {
-        return (-1);
-            close(fd);
+      return (-1);
+      close(fd);
     }
-        close(fd);
-        return (1);
+    close(fd);
+    return (1);
 }
