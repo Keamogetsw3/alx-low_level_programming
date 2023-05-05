ffce077bc8c7576d8883b4ae92236fe948319aa0
keamogetswe3 <keamogetswegp@gmail.com>
2023-05-05 15:33:32 -0700

    fix function

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,11 +7,11 @@
  *
  *Return:1 if it worked, or -1 if an error occurred
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	*n |= ~(1 << index);
+	*n &= ~(1 << index);
 
 	return (1);
 }
