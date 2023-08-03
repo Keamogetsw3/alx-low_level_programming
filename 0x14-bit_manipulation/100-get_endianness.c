9054a309a3805745e0b78fef38d84475a2fd9fce
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-03 21:04:42 +0200

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * get_endianness - checks the endianness
+ *
  * Return: 1 on little endian,  return 0 on big endian
  */
 int get_endianness(void)
 {
-    int num;
-    char *endian;
+	int num;
+	char *endian;
 
-    num = 1;
-    endian = (char*)&num;
-    return (*endian == 1);
+	num = 1;
+	endian = (char *)&num;
+	return (*endian == 1);
 }
