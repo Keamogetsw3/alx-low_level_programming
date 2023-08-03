8432648653961b011c5a31c284651a741dc7dd81
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-03 20:59:33 +0200

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,13 +5,14 @@
  * @n: bitnum_1
  * @m: bitnum_2
  *
- * Return:  number of bits you would need to flip to get from one number to another
+ * Return:  number of bits you would need to flip to get from 
+ * one number to another
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int bitxor;
 	unsigned int nflips;
-	
+
 	bitxor = n ^ m;
 	nflips = 0;
 	while (bitxor != 0)
