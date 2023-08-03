12dfa765ee01a84a9f578a27fb9648de793939f6
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-03 20:10:37 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,18 @@
  *@b: pointing to a string of 0 and 1 chars
  * Return: the converted number, or 0
  */
- unsigned int binary_to_uint(const char *b)
- {
-     unsigned int dec = 0;
-     int len;
-     
-     if (b == NULL)
-        return (0);
-    for (len = 0; b[len] != '\0'; len++)
-    {
-        if (b[len] < '0' || b[len] > '1')
-	  return (0);
-        dec = dec * 2 + (b[len] - '0');
-    }
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int len;
 
-    return (dec);
+	if (b == NULL)
+		return (0);
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		if (b[len] < '0' || b[len] > '1')
+			return (0);
+		dec = dec * 2 + (b[len] - '0');
+	}
+	return (dec);
 }
