d4101fa2c47c2188896f3e6366d7e50ac549889b
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-18 15:36:22 -0700

    ds a new node at the end of a dlistint_t list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,1 +1,35 @@
 #include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+  dlistint_t *new_node;
+	dlistint_t *current_n;
+
+	new_node = malloc(sizeof(dlistint_t));
+	current_n = *head;
+
+	if (head == NULL)
+		return (NULL);
+	
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+
+	while (current_n->next != NULL)
+		current_n = current_n->next;
+	current_n->next = new_node;
+	new_node->prev = current_n;
+	return (new_node);
+}
