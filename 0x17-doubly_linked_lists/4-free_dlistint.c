ed7107c6b99cd558b405c2b47382cb6b7a7d3127
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-18 15:44:15 -0700

    frees a dlistint_t list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+void free_dlistint(dlistint_t *head)
+{
+  dlistint_t *current_n;
+
+	while (head)
+	{
+		current_n = head;
+		head = head->next;
+		free(current_n);
+	}
+}
