f90a357b006222021329fec74c1e9f861f0b3d64
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-19 01:05:21 +0200

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,23 +5,19 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index) 
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    dlistint_t *current_n;
-    unsigned int counter;
+	dlistint_t *current_n;
+	unsigned int count;
 
-    current_n = head;
-    counter = 0;
-
-    while (current_n != NULL)
-    {
-        if (counter == index)
-        {
-            return (current_n);
-        }
-        current_n = current_n->next;
-        counter++;
-    }
-
-    return NULL;
+	current_n = head;
+	count = 0;
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		current = current->next;
+		count++;
+	}
+	return (NULL);
 }
