6319c38b9142b53f2ceb707e39e3bf3c2cb1cf6c
keamogetswe3 <keamogetswegp@gmail.com>
2023-08-18 16:22:10 -0700

    returns the sum of all the data (n) of a dlistint_t linked list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,18 @@
 #include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+  int sum = 0;
+
+  while (head != NULL)
+    {
+      sum += head->n;
+      head = head->next;
+    }
+  return (sum);
+}
