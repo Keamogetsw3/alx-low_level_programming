19283be46ac441af28e7aece54835fc62eeaeedc
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-22 04:16:06 +0200

    Update _strcmp.c

--- 0x18-dynamic_libraries/_strcmp.c
+++ 0x18-dynamic_libraries/_strcmp.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 /**
-	* function: _strcmp
-	* @:
-	* @:
-	*
-	* Return:
-**/
-int _strcmp()
+  * _strcmp - compares values of two string
+  * @s1: first string to compare
+  * @s2: second string to compare
+  *
+  * Return: 0 on success
+ */
+int _strcmp(char *s1, char *s2)
 {
-    // TODO: Implement the function
-    return 0;
+	int index;
+
+	for (index = 0; s1[index] != '\0' && s2[index] != '\0'; index++)
+	{
+		if (s1[index] != s2[index])
+		{
+			return ((int)s1[index] - s2[index]);
+		}
+	}
+	return (0);
 }
