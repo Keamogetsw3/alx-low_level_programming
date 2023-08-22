7b4efa35d9e3a9a575c53e1d7debe5fc5d7981cd
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-22 06:11:50 +0200

    Update _strstr.c

--- 0x18-dynamic_libraries/_strstr.c
+++ 0x18-dynamic_libraries/_strstr.c
@@ -1,41 +1,5 @@
 #include "main.h"
 
-/**
-	* _strstr: find the first occurrence of a substring in a string
-	* @haystack:
-	* @needle:
-	*
-	* Return:
-**/
-char *_memcpy(char *dest, char *src, unsigned int n) {
-    char *dest_start = dest;
-
-    while (n > 0) {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
-
-    return dest_start;
-}
-
-/* Function to search a string for any of a set of characters */
-char *_strpbrk(char *s, char *accept) {
-    while (*s != '\0') {
-        char *a = accept;
-        while (*a != '\0') {
-            if (*s == *a) {
-                return s;
-            }
-            a++;
-        }
-        s++;
-    }
-
-    return NULL;
-}
-
 /* Function to find the first occurrence of a substring in a string */
 char *_strstr(char *haystack, char *needle)
 {
