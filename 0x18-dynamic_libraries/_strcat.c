ead03e35de2b85ced2f03b741bcef369632cfe9a
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-22 04:07:27 +0200

    Create the dynamic library libdynamic.so containing all the functions

--- 0x18-dynamic_libraries/_strcat.c
+++ 0x18-dynamic_libraries/_strcat.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
 /**
-	* function: _strcat
-	* @:
-	* @:
-	*
-	* Return:
-**/
-int _strcat()
+  * _strcat - concatenate strimgs
+  * @dest: first string to concatenate
+  * @src: second string to concatenate
+  *
+  * Return: pointer to destination string
+ */
+char *_strcat(char *dest, char *src)
 {
-    // TODO: Implement the function
-    return 0;
+	int pos_source;
+	int pos_2;
+
+	pos_source = 0;
+	pos_2 = 0;
+	while (src[pos_2] != '\0')
+	{
+		pos_2++;
+	}
+	while (src[pos_source] != '\0')
+	{
+		dest[pos_source] = src[pos_source];
+		pos_2++;
+		pos_source++;
+	}
+	dest[pos_2] = '\0';
+	return (dest);
 }
