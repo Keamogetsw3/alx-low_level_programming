be9f764bf57ac52f0d5795df69991de96364d877
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-22 04:40:39 +0200

    Update _strncat.c

--- 0x18-dynamic_libraries/_strncat.c
+++ 0x18-dynamic_libraries/_strncat.c
@@ -1,14 +1,31 @@
 #include "main.h"
 
 /**
-	* function: _strncat
-	* @:
-	* @:
+	* _strncat: concatenate (append) the first n characters of src to dest
+	* @dest:
+	* @src:
+ *@n:
 	*
 	* Return:
 **/
-int _strncat()
+char *_strncat(char *dest, char *src, int n) 
 {
-    // TODO: Implement the function
-    return 0;
+    char *dest_start = dest;
+
+    while (*dest != '\0') 
+    {
+        dest++;
+    }
+
+    while (*src != '\0' && n > 0) 
+    {
+        *dest = *src;
+        dest++;
+        src++;
+        n--;
+    }
+
+    *dest = '\0';
+
+    return (dest_start);
 }
