b57135db034f6c3e6d32a406ca86992a8a04a6b2
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-26 05:46:05 +0200

    Update 100-operations.c

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,48 +1,48 @@
 #include "main.h"
 
 /**
- * _add - Adds two intergers
+ * add - Adds two intergers
  * @a:the first interger
  * @b:the second interger
  * 
  * Return: the results of the addition
  */
-int _add(int a, int b)
+int add(int a, int b)
 {
   return (a + b);
 }
 
 /**
- * _sub - substract two intergers
+ * sub - substract two intergers
  * @a: the firdt interger
  * @b: the second interger
  * 
  * Return: the results of the substraction 
  */
-int _sub(int a, int b)
+int sub(int a, int b)
 {
   return (a - b);
 }
 
 /**
- * _mul - multiply two intergers
+ * mul - multiply two intergers
  * @a: the first interger
  * @b: the second interger
  * 
  * Return: the product
  */
-int _mul(int a, int b)
+int mul(int a, int b)
 {
   return (a * b);
 }
 
 /**
- * _div - divide two intergers
+ * div - divide two intergers
  * @a: first integer
  * @b: second integers
  * Return: the results of division
  */
-int _div(int a, int b)
+int div(int a, int b)
 {
   if (b == 0)
     {
@@ -53,13 +53,13 @@ int _div(int a, int b)
 }
 
 /**
-* _mod - calculate
+* mod - calculate
 * @a: first int
 * @b: second int
 *
 * Return: results
 */
-int _mod(int a, int b)
+int mod(int a, int b)
 {
     if (b == 0)
       {
