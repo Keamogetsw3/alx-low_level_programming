a03ebb90257d0693b72c119010ade5b256b6e94f
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-08-26 05:46:39 +0200

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include <stddef.h>
 
-int _add(int a, int b);
-int _sub(int a, int b);
-int _mul(int a, int b);
-int _div(int a, int b);
-int _mod(int a, int b);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 
 int _putchar(char c);
 int _islower(int c);
