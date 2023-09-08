9226ce2877369949db0a735a043a5f0002d9f80d
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-09-09 01:20:41 +0200

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,9 +3,10 @@
 /**
  *  hash_table_set - adds an element to the hash table.
  *  @ht: hash table you want to add or update the key/value to
- *  @variable2:
+ *  @key:  is the key. key can not be an empty string
+ *  @value:  value associated with the key. value must be duplicated. value can be an empty string
  *
- *  Return: what the function returns
+ *  Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
