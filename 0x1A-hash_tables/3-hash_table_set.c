0bda0832e946b7e4c32ecea6ddf2cd2a62a044a1
Keamogetsw3 <keamogetswegp@gmail.com>
2023-09-08 16:16:55 -0700

    Write a function that adds an element to the hash ta

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,51 @@
 #include "hash_tables.h"
 
 /**
- *  function - describe what the function does
- *  @variable1: the first variable
+ *  hash_table_set - adds an element to the hash table.
+ *  @ht: hash table you want to add or update the key/value to
  *  @variable2:
  *
  *  Return: what the function returns
  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	char *value_copy;
+	unsigned long int index, i;
 
-/* Your C code here */
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
