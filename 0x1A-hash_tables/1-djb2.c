0bf5d2b0d80d425b1850691f29e972e882a75073
Keamogetsw3 <keamogetswegp@gmail.com>
2023-09-08 11:28:18 -0700

    WHITESPCA REMOVED

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,11 +1,11 @@
-								/**
-								 *  function - describe what the function does
-								 *  @variable1: the first variable
-								 *  @variable2:
-								 *
-								 *  Return: what the function returns
-								 */
-								
-								#include "hash_tables.h"
-								
-								/* Your C code here */
+#include "hash_tables.h"
+
+/**
+ *  function - describe what the function does
+ *  @variable1: the first variable
+ *  @variable2:
+ *
+ *  Return: what the function returns
+ */
+
+/* Your C code here */
