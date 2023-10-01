994ea5b6fc7945a1632fdb9f7e71e5c5d31df92a
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-10-01 22:11:15 +0200

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,34 @@
 #include "hash_tables.h"
 
 /**
- *  function - describe what the function does
- *  @variable1: the first variable
- *  @variable2:
- *
- *  Return: what the function returns
+ * hash_table_delete - Deletes a hash table and all its elements.
+ * @ht: A pointer to the hash table to be deleted.
+ * 
+ * Return: Nothing
  */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_table_t *head = ht;
+    hash_node_t *current_node, *next_node;
+    
 
-/* Your C code here */
+    for (i = 0; i < ht->size; i++)
+    {
+        if (ht->array[index] != NULL)
+        {
+            current_node = ht->array[i];
+            while (current_node != NULL)
+            {
+                next_node = current_node->next;
+                free(current_node->key);
+                free(current_node->value);
+                free(current_node);
+                current_node = next_node;
+            }
+        }
+    }
+    
+    free(head->array);
+    free(head);
+}
