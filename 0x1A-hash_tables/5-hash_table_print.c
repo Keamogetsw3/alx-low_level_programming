5abb46163a610bffcd077919d7e5bfb1dbeb58de
Keamogetsw3 <122805140+Keamogetsw3@users.noreply.github.com>
2023-10-01 21:59:33 +0200

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,32 @@
 #include "hash_tables.h"
 
 /**
- *  function - describe what the function does
- *  @variable1: the first variable
- *  @variable2:
- *
- *  Return: what the function returns
+ * hash_table_print - Prints the keys and values of the hash table.
+ * @ht: A pointer to the hash table.
+ * 
+ * Return: nothing
  */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *current_node;
+    char *sep;
 
-/* Your C code here */
+    if (ht == NULL)
+        return;
+
+    printf("{");
+    sep = "";
+
+    for (i = 0; i < ht->size; i++)
+    {
+        current_node = ht->array[i];
+        while (current_node != NULL)
+        {
+            printf("%s'%s': '%s'", sep, current_node->key, current_node->value);
+            sep = ", ";
+            current_node = current_node->next;
+        }
+    }
+    printf("}\n");
+}
